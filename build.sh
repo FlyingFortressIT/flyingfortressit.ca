89e708fcb88f30d5f60986ee8c7dca17d40fb84b
Mike Himbeault <mike@riebart.ca>
2017-07-06 15:16:22 -0500

    Adding `aws s3 cp` to the build script.

--- build.sh
+++ build.sh
@@ -11,3 +11,4 @@ fi
 
 rm -rf public 2>/dev/null
 $HUGO_BIN --ignoreCache
+aws s3 cp --recursive --exclude '.git/*' --exclude '.gitignore' --exclude '.gitmodules' --dryrun . s3://ffit-static-website-content/website/
